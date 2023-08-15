33e21954cc04addc262c91e4eef0c69a12df2d52
Gabriela Hermenegildo Júnior <gabriela.hermenegil@meta.com.br>
2023-08-15 10:29:00 -0300

    adding spring project

---
+++ backend/src/main/java/com/songsplayer/backend/SongsPlayerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.songsplayer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsPlayerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsPlayerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/songsplayer/backend/SongsPlayerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songsplayer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsPlayerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
